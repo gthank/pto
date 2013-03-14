fc81a2a2cfcc5975942663a88624eba9123bf2cd
Hank Gay <hank.gay@gmail.com>
2013-03-13 23:06:44 -0400

    Update trove classifiers.

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.3',
         'Programming Language :: Python :: 2.4',
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
