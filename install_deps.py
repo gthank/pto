32de58c3a36bf9f9f8ec98e904aee989ebe3428e
Hank Gay <hank.gay@gmail.com>
2013-03-14 21:54:25 -0400

    Install different deps for Python 3 vs. Python 2.

---
+++ install_deps.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Installs dependencies appropriate for the Python version."""
+import subprocess
+import sys
+
+subprocess.call("pip", "install --use-mirrors -r requirements.txt")
+if sys.version_info[0] >= 3: # Python 3
+    # No Python 3-specific dependencies right now
+    pass
+else:  # Python 2
+    subprocess.call("pip", "install --use-mirrors unittest2")
