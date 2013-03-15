ca712993b2ddfc83e7ec47b88ae5520148ef9939
Hank Gay <hank.gay@gmail.com>
2013-03-14 21:59:05 -0400

    Fix my subprocess.call command.

--- install_deps.py
+++ install_deps.py
@@ -4,9 +4,9 @@
 import subprocess
 import sys
 
-subprocess.call("pip", "install --use-mirrors -r requirements.txt")
+subprocess.call(["pip", "install", "--use-mirrors", "-r", "requirements.txt"])
 if sys.version_info[0] >= 3: # Python 3
     # No Python 3-specific dependencies right now
     pass
 else:  # Python 2
-    subprocess.call("pip", "install --use-mirrors unittest2")
+    subprocess.call(["pip", "install", "--use-mirrors", "unittest2"])
